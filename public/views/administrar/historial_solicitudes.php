da9f5140761a1f8c23cb8e684ca3a7b91a6e670f
Norbert Stange <norberto.stange@est.fi.uncoma.edu.ar>
2021-04-23 13:35:03 -0300

    Actualización historial solicitudes
    -  Prueba del botón imprimir solo para las solicitudes aprobadas

--- public/views/administrar/historial_solicitudes.php
+++ public/views/administrar/historial_solicitudes.php
@@ -126,7 +126,7 @@ $solicitudesAprobadas = [];
 <script src="../../../node_modules/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
 
-<script src="../../js/administrar/nuevas_solicitudes.js"></script>
+
 <script src="../../js/administrar/periodo_solicitudes.js"></script>
 
 </html>
