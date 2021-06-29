12140bbbebdf5667630f4d6695da93a78798f118
Norbert Stange <n0rb3r7@outlook.com>
2021-06-29 12:15:02 -0300

    Cambio nombre del csv que se descarga y limpieza de descarga_periodo_csv.php

--- public/views/administrar/descarga_periodo_csv.php
+++ public/views/administrar/descarga_periodo_csv.php
@@ -8,26 +8,7 @@ if (isset($_POST['fecha_desde']) and isset($_POST['fecha_hasta'])) {
     $fecha_desde = str_replace("/", "-", $_POST['fecha_desde']);
     $fecha_hasta = str_replace("/", "-", $_POST['fecha_hasta']);
     $solicitudPeriodo = $solicitudController->getSolicitudesWherePeriodApproved($_POST['fecha_desde'], $_POST['fecha_hasta']);
-    //print_r($fecha_hasta);
-    //die();
-    //echo (utf8_converter($solicitudPeriodo, true));
 
-    $file_name = 'solicitudes-'. $fecha_desde.'-'. $fecha_hasta.'.csv';
-    $csv = '.csv';
-    $date = date('Y-m-d');
-    header("Content-Type: text/csv;charset=utf-8");
-    header("Content-Disposition: attachment;filename=\"$file_name$csv\"");
-    header("Content-Transfer-Encoding: binary");
-    header("Pragma: no-cache");
-    header("Expires: 0");
-    header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
-    header("Last-Modified: {$date} GMT");
-    header("Content-Type: application/force-download");
-    header("Content-Type: application/octet-stream");
-    header("Content-Type: application/download");
-
-    //$fp = fopen('php://output' . 'data.csv', 'w');
-    //$fp = fopen("./csv/" . "".$file_name."", "w+");
     $fp = fopen("./csv/" . 'solicitudes.csv', "w+");
     $header = [
         'numero solicitud', 'nombre solicitante', 'dni', 'fecha nacimiento', 'direccion', 'telefono', 'telefono actualizado', 'email', 'email actualizado', 'tipo empleo', 'renovacion', 'numero recibo', 'fecha expedicion', 'fecha vencimiento', 'observaciones', 'admin evaluador', 'retira en', 'estado'
